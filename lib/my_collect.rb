ba8575bed26c0c2db1b5096c51d23b8aff1ab805
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-10 18:08:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,15 @@
 def my_collect(array)
   
   i = 0 
+  collection = []
   while i < array.length 
-  
+  collection << yield(array[i])
+  i = i + 1
+end
+collection
+end
+
+my_collect(array) do |name|
+  name.split(" ".last)
+end
 
